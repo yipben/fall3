199b40ea4d095a4d6c45584a5180ac8111812a75
Ben Yip <bmyip@ncsu.edu>
2018-11-12 11:38:41 -0500

    adding language detection

--- clust/proj1_start.R
+++ clust/proj1_start.R
@@ -1,8 +1,11 @@
 library(tm)
+library(cld2)
 library(readr)
 library(dplyr)
 library(tidytext)
 library(text2vec)
+library(SnowballC)
+library(wordcloud)
 library(factoextra)
 
 
@@ -19,16 +22,39 @@ three_plus <- rev %>%
   filter(n > 3) %>%
   pull(listing_id)
 rev <- rev %>%
-  filter(listing_id %in% three_plus)
+  filter(listing_id %in% three_plus) %>%
+  mutate(lang = detect_language(comments))
+
+# add specific stop words
+airbnb_stop_words <- c("boston", "airbnb", "host", "hosts",
+                       "apartment", "house", "stay", "time",
+                       "bnb", "city", "street", "night", "stayed")
+airbnb_stop_words <- data_frame(word = airbnb_stop_words,
+                                lexicon = "custom")
+stop_words2 <- rbind(stop_words, airbnb_stop_words)
 
 # df w/ one word per row
 word_per_row <- rev %>%
+  filter(lang == "en") %>%
   unnest_tokens(word, comments) %>%
-  anti_join(stop_words) %>%
-  filter(!is.na(word))
+  anti_join(stop_words2) %>%
+  filter(!is.na(word)) 
+stem_per_row <- rev %>%
+  filter(lang == "en") %>%
+  unnest_tokens(word, comments) %>%
+  anti_join(stop_words2) %>%
+  filter(!is.na(word)) %>%
+  mutate(word = wordStem(word))
+
+# wordcloud
+word_counts <- word_per_row %>%
+  group_by(word) %>%
+  summarise(count = n()) %>%
+  arrange(desc(count))
+wordcloud(word_counts$word, word_counts$count, max.words = 50)
 
 # creating dtm
-dtm <- word_per_row %>%
+dtm <- stem_per_row %>%
   count(listing_id, word) %>%
   cast_dtm(listing_id, word, n)
 # inspect(dtm) # uncomment line to view contents of dtm
@@ -52,7 +78,7 @@ score_loc_df <- list %>%
          avg_score = scale(avg_score))
 set.seed(4151)
 score_loc_samp <- score_loc_df %>%
-  sample_n(100)
+  sample_n(200)
   
 
 # CLUSTERING ==================================================================
@@ -61,14 +87,14 @@ score_loc_samp <- score_loc_df %>%
 
 # calculating distances
 cos <- 1 - sim2(m, method = "cosine", norm = "l2")
-# jac <- 1 - sim2(as.matrix(dtm), method = "jaccard", norm= "none")
+# jac <- 1 - sim2(as.matrix(dtm), method = "jaccard", norm = "none")
 
-# selecting k
-fviz_nbclust(m_samp, hcut, k.max = 10, method = "wss") # no clear elbow
-fviz_nbclust(m_samp, hcut, k.max = 10, nboot = 20, method = "gap") # no drop at 10
-fviz_nbclust(m_samp, hcut, k.max = 10, method = "silhouette") # 2 clusters
+# selecting k (takes long time to b/c so many cols.)
+fviz_nbclust(m_samp, hcut, k.max = 10, method = "wss") 
+fviz_nbclust(m_samp, hcut, k.max = 10, nboot = 20, method = "gap") 
+fviz_nbclust(m_samp, hcut, k.max = 10, method = "silhouette") 
 
-fviz_nbclust(m_samp, kmeans, k.max = 20, method = "wss") # no clear elbow
+fviz_nbclust(m_samp, kmeans, k.max = 20, method = "wss") 
 fviz_nbclust(m_samp, kmeans, k.max = 10, nboot = 10, method = "gap")
 fviz_nbclust(m_samp, kmeans, k.max = 20, method = "silhouette") 
 
@@ -79,6 +105,10 @@ l2dist <- score_loc_df %>%
   dist(method = "euclidean") 
 
 # selecting k
-fviz_nbclust(l2dist, hcut, k.max = 20, method = "wss") # no clear elbow
-fviz_nbclust(m_samp, hcut, k.max = 30, nboot = 10, method = "gap") # 
-fviz_nbclust(m_samp, hcut, k.max = 20, method = "silhouette")
+fviz_nbclust(score_loc_samp[, 2:4], hcut, k.max = 20, method = "wss") # no clear elbow
+fviz_nbclust(score_loc_samp[, 2:4], hcut, k.max = 20, method = "gap") # drops after 3
+fviz_nbclust(score_loc_samp[, 2:4], hcut, k.max = 20, method = "silhouette") # drop after 3
+
+fviz_nbclust(score_loc_samp[, 2:4], kmeans, k.max = 20, method = "wss") 
+fviz_nbclust(score_loc_samp[, 2:4], kmeans, k.max = 20, method = "gap") # drops after 2
+fviz_nbclust(score_loc_samp[, 2:4], kmeans, k.max = 20, method = "silhouette") 
