4ade8bd675ad515f2f2279e460ba732c950aff10
Ben Yip <bmyip@ncsu.edu>
2018-11-13 22:21:59 -0500

    adding function for plotting wordclouds

--- clust/proj1_final.R
+++ clust/proj1_final.R
@@ -1,5 +1,6 @@
 library(tm)
 library(cld2)
+library(purrr)
 library(readr)
 library(dplyr)
 library(tidytext)
@@ -27,10 +28,11 @@ rev <- rev %>%
 # add specific stop words
 airbnb_stop_words <- c("boston", "airbnb", "host", "hosts",
                        "apartment", "house", "stay", "time",
-                       "bnb", "city", "street", "night", "stayed")
-airbnb_stop_words <- data_frame(word = airbnb_stop_words,
-                                lexicon = "custom")
-stop_words2 <- rbind(stop_words, airbnb_stop_words)
+                       "bnb", "city", "street", "night", "stayed",
+                       "staying", tolower(list$host_name))
+airbnb_stop_words_df1 <- data_frame(word = airbnb_stop_words,
+                                    lexicon = "custom")
+stop_words2 <- rbind(stop_words, airbnb_stop_words_df1)
 
 # df w/ one word per row
 word_per_row <- rev %>%
@@ -39,12 +41,12 @@ word_per_row <- rev %>%
   anti_join(stop_words2) %>%
   filter(!is.na(word)) 
 
-# wordcloud
+# wordcloud of all reviews
 word_counts <- word_per_row %>%
   group_by(word) %>%
   summarise(count = n()) %>%
   arrange(desc(count))
-wordcloud(word_counts$word, word_counts$count, max.words = 50)
+wordcloud(word_counts$word, word_counts$count, max.words = 20)
 
 # Review scores (from air bnb) ------------------------------------------------
 
@@ -101,6 +103,19 @@ clusters %>%
   summarise(avg_price_per = mean(adj_price), avg_rating = mean(review_scores_rating),
             avg_loc_rating = mean(review_scores_location), count = n())
 
+# remove top 10 words to create more interesting clouds
+common_words <- word_per_row %>%
+  left_join(clusters, by = c("listing_id" = "id")) %>% 
+  group_by(word) %>%
+  summarise(count = n(), n_clust = length(unique(hier9))) %>%
+  arrange(desc(count)) %>%
+  filter(n_clust == 9) %>%
+  select(word) %>%
+  pull()
+
+word_per_row2 <- word_per_row %>%
+  filter(!(word %in% common_words[1:20]))
+
 # compare wordclouds
 hier9_words <- clusters %>%
   select(id, hier9) %>%
@@ -109,33 +124,21 @@ hier9_words <- clusters %>%
   group_by(hier9, word) %>%
   summarise(count = n())
 
+plot_wordcloud <- function(x, color = T) {
+  if (color) {
+    hier9_words %>%
+      filter(hier9 == x) %>%
+      with(wordcloud(word, count, max.words = 30, colors = brewer.pal(9,"BuGn")))
+  } else {
+    hier9_words %>%
+      filter(hier9 == x) %>%
+      with(wordcloud(word, count, max.words = 30))
+  }  
+}
+
 par(mfrow = c(3, 3), mar = rep(0, 4))
-hier9_words %>%
-  filter(hier9 == 1) %>%
-  with(wordcloud(word, count, max.words = 50, colors = brewer.pal(50,"BuGn")))
-hier9_words %>%
-  filter(hier9 == 2) %>%
-  with(wordcloud(word, count, max.words = 50, colors = brewer.pal(50,"BuGn")))
-hier9_words %>%
-  filter(hier9 == 3) %>%
-  with(wordcloud(word, count, max.words = 50, colors = brewer.pal(50,"BuGn")))
-hier9_words %>%
-  filter(hier9 == 4) %>%
-  with(wordcloud(word, count, max.words = 50, colors = brewer.pal(50,"BuGn")))
-hier9_words %>%
-  filter(hier9 == 5) %>%
-  with(wordcloud(word, count, max.words = 50, colors = brewer.pal(50,"BuGn")))
-hier9_words %>%
-  filter(hier9 == 6) %>%
-  with(wordcloud(word, count, max.words = 50, colors = brewer.pal(50,"BuGn")))
-hier9_words %>%
-  filter(hier9 == 7) %>%
-  with(wordcloud(word, count, max.words = 50, colors = brewer.pal(50,"BuGn")))
-hier9_words %>%
-  filter(hier9 == 8) %>%
-  with(wordcloud(word, count, max.words = 50, colors = brewer.pal(50,"BuGn")))
-hier9_words %>%
-  filter(hier9 == 9) %>%
-  with(wordcloud(word, count, max.words = 50, colors = brewer.pal(50,"BuGn")))
-dev.off()
+map(as.list(1:9), ~ plot_wordcloud(.x, color = F))
+
+
+
 
