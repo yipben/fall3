a09a7525fee56600580a790baf5295651849c85e
Ben Yip <bmyip@ncsu.edu>
2018-11-21 20:43:08 -0500

    adding code for bootstrap

--- sim/full_sim.R
+++ sim/full_sim.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(purrr)
 library(tictoc)
+library(ggplot2)
 
 source("hw2_sim_only.R")
 
@@ -85,6 +86,95 @@ ggplot() +
   ) +
   theme(text = element_text(size = 18))
 
-mean(one_mil_sim_results$npv, na.rm = T)
-sum(is.na(one_mil_sim_results$npv))
-summary(one_mil_sim_results$npv)
+# mean(one_mil_sim_results$npv, na.rm = T)
+# sum(is.na(one_mil_sim_results$npv))
+# summary(one_mil_sim_results$npv)
+
+
+# BOOTSTRAPPING FOR CI's ======================================================
+
+boot <- function(stat = "var") {
+  
+  s <- sample(one_mil_sim_results$npv, size = 1000000, replace = T)
+  
+  if (stat == "var") {
+    
+    quantile(s, .05, na.rm = T)
+    
+  } else if (stat == "es") {
+    
+    s_sort <- sort(s)
+    
+    mean(s_sort[1:(1000000 * .05)])
+    
+  }
+}
+
+# 1,000 takes ~00:01:00 | 10,000 takes ~ 00:10:30 |
+tic()
+set.seed(8)
+VaR_estimates <- unlist(rerun(10000, boot()))
+toc()
+
+# 1,000 takes ~00:02:21 | 10,000 takes ~ 00:10:30 |
+tic()
+set.seed(8)
+es_estimates <- unlist(rerun(1000, boot(stat = "es")))
+toc()
+# =============================================================================
+
+
+# PLOT BOOTSTRAP RESULTS ------------------------------------------------------
+
+# value at risk
+ggplot() + 
+  geom_histogram(
+    aes(VaR_estimates/1000000), 
+    bins = 50,
+    colour = "black", 
+    fill = "sky blue",
+    alpha = .5
+  ) +
+  labs(
+    x = "Value at Risk (millions)",
+    y = "Frequency",
+    title = "Bootstrap results for Value at Rist (VaR)"
+  ) +
+  theme(
+    text = element_text(size = 18)
+  ) +
+  geom_vline(
+    xintercept = quantile(VaR_estimates/1000000, .05),
+    linetype = "dashed"
+  ) +
+  geom_vline(
+    xintercept = quantile(VaR_estimates/1000000, .95),
+    linetype = "dashed"
+  )
+
+# expected shortfall
+ggplot() + 
+  geom_histogram(
+    aes(es_estimates/1000000), 
+    bins = 50,
+    colour = "black", 
+    fill = "sky blue",
+    alpha = .5
+  ) +
+  labs(
+    x = "Expected Shortfall (millions)",
+    y = "Frequency",
+    title = "Bootstrap results for Expected Shortfall (ES)"
+  ) +
+  theme(
+    text = element_text(size = 18)
+  ) +
+  geom_vline(
+    xintercept = quantile(es_estimates/1000000, .05, na.rm = T),
+    linetype = "dashed"
+  ) +
+  geom_vline(
+    xintercept = quantile(es_estimates/1000000, .95, na.rm = T),
+    linetype = "dashed"
+  )
+
